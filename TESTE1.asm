93d449682c40ded34dddfc4c078adab7bd3dcbbf
Zaldef <guilhermeroelli@gmail.com>
2022-10-26 21:55:05 -0300

    1

--- TESTE1.asm
+++ TESTE1.asm
@@ -2,25 +2,15 @@ TITLE Guilherme Roelli (22899140) & Vitor Yuzo Takei (22023740)
 .MODEL SMALL
 .CODE
     MAIN PROC
-    MOV BH,7
-    MOV BL,2
-        DAUX1:
-            SHL BH,4
-        DAUX2:
-            SHL BL,4
-        DAUX3:
-            CMP BH,BL
-            JAE DAUX4
-            JB FIM
-            JMP DAUX3
-        DAUX4:
-            SUB BH,BL
-            ADD CL, 1
-            JMP DAUX3
-            CMP BH,0
-            JE FIM
-            JMP DAUX3
-
+        MOV AH,01h
+        INT 21h
+        MOV BL,AL
+        MOV AH,02h
+        MOV DL,10
+        INT 21h
+        MOV AH,02h
+        MOV DL,BL
+        INT 21H
 
     FIM:
     MOV AH,4Ch         
